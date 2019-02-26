15a153bb12d5b8fd9652291412f8b33cf7527f1c
Marcin Migdal <marcinmigdal@wp.pl>
2019-02-27 00:12:17 +0100

    Code cleanup
    Signed-off-by: Marcin Migdal <marcinmigdal@wp.pl>

--- src/main/java/aniela/remanent/pdf/report/abstraction/ReportPdf.java
+++ src/main/java/aniela/remanent/pdf/report/abstraction/ReportPdf.java
@@ -229,23 +229,14 @@ public abstract class ReportPdf implements ReportPdfApi {
         }
 
         public static String formatAmount(double amount) {
-            // System.out.println("====================");
-            // System.out.println("Amount: " + amount);
             BigDecimal bigDecimal = new BigDecimal(amount);
             int intValue = bigDecimal.intValue();
             BigDecimal delimiter = bigDecimal.subtract(new BigDecimal(intValue));
-            // System.out.println("Double Number: " + bigDecimal.toPlainString());
-            // System.out.println("Integer Part: " + intValue);
-            // System.out.println("Delimiter Part: " + delimiter.toPlainString());
             if (delimiter.equals(BigDecimal.valueOf(0))) {
-                //   System.out.println("Zwracam Inta");
                 return String.valueOf(intValue);
             } else {
-                //  System.out.println("Zwracam calosc");
                 return String.valueOf(amount);
             }
         }
     }
-
-
 }
