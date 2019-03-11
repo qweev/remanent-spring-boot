48ba867e15ba77a62ec2835b9b9e89160d25deb1
Marcin Migdal <marcinmigdal@wp.pl>
2019-03-11 14:14:08 +0100

    Fix endpoint for Excel report
    Signed-off-by: Marcin Migdal <marcinmigdal@wp.pl>

--- src/main/java/aniela/remanent/raport/RaportREST.java
+++ src/main/java/aniela/remanent/raport/RaportREST.java
@@ -106,7 +106,7 @@ public class RaportREST {
 
 	}
 
-	@GetMapping("/remanent/rest/raportNetto/zaloguj/{user}/{pass}")
+    @GetMapping("/remanent/rest/raport/zaloguj/{user}/{pass}")
 	public ResponseEntity sprawdzLoginHaslo(@PathVariable("user") String user, @PathVariable("pass") String pass) {
 		logger.info("pobrany login : " + user);
 		logger.info("pobrany pass : " + pass);
