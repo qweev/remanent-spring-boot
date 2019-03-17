92a68426e99f575af18015b5463fb3274fa3abe9
Marcin Migdal <marcinmigdal@wp.pl>
2019-03-17 21:02:07 +0100

    Improvements related to naming convention
    Signed-off-by: Marcin Migdal <marcinmigdal@wp.pl>

--- src/main/java/aniela/remanent/pdf/RestControllerReportPdf.java
+++ src/main/java/aniela/remanent/pdf/RestControllerReportPdf.java
@@ -5,7 +5,6 @@ import aniela.remanent.pdf.report.netto.ReportPdfNetto;
 import aniela.remanent.position.abstraction.PositionInterface;
 import aniela.remanent.pozycje.BazaDAO;
 import aniela.remanent.raport.ReportFileResolver;
-import java.util.List;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +12,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class RestControllerReportPdf {
@@ -29,27 +30,24 @@ public class RestControllerReportPdf {
     @Autowired
     ReportPdfNetto reportPdfNetto;
 
-    @GetMapping("/remanent/rest/raport/pdf/brutto/{sciezka}")
-    public ResponseEntity utworzPlikRemanentBrutto(@PathVariable("sciezka") String sciezka) {
-        String fullSciezka = ReportFileResolver.resolveFilePathForPdf(sciezka);
+    @GetMapping("/remanent/rest/raport/pdf/brutto/{nazwaPliku}")
+    public ResponseEntity utworzPlikRemanentBrutto(@PathVariable("sciezka") String nazwaPliku) {
+        String pathAfterResolve = ReportFileResolver.resolveFilePathForPdf(nazwaPliku);
         try {
-
             List<PositionInterface> positions = bazaDAO.przygotujPozycjeDoRaportuBrutto();
-            reportPdfBrutto.generateReport(positions, fullSciezka, positions.size());
+            reportPdfBrutto.generateReport(positions, pathAfterResolve, positions.size());
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(String.format("Report generation has thrown error %s", e.getMessage()));
         }
         return ResponseEntity.status(HttpStatus.OK).body("OK");
     }
 
-    @GetMapping("/remanent/rest/raport/pdf/netto/{sciezka}")
-    public ResponseEntity utworzPlikRemanentNetto(@PathVariable("sciezka") String sciezka) {
-        String fullSciezka = ReportFileResolver.resolveFilePathForPdf(sciezka);
+    @GetMapping("/remanent/rest/raport/pdf/netto/{nazwaPliku}")
+    public ResponseEntity utworzPlikRemanentNetto(@PathVariable("nazwaPliku") String nazwaPliku) {
+        String pathAfterResolve = ReportFileResolver.resolveFilePathForPdf(nazwaPliku);
         try {
-
             List<PositionInterface> positions = bazaDAO.przygotujPozycjeDoRaportuNetto();
-
-            reportPdfNetto.generateReport(positions, fullSciezka, positions.size());
+            reportPdfNetto.generateReport(positions, pathAfterResolve, positions.size());
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(String.format("Report generation has thrown error %s", e.getMessage()));
         }
